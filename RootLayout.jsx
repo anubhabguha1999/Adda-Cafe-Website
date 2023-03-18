26e77c4271266f2e7e658ba367a8be04d508d610
debarkamondal <debarkamondal@gmail>
2023-03-18 11:51:06 +0530

    upgraded from react-router-dom v6.2 to v6.9 which provides new api

---
+++ RootLayout.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from "react";
+import { Outlet } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import { Header } from "./components";
+import { useStateValue } from "./context/StateProvider";
+import { getAllFoodItems } from "./utils/firebaseFunctions";
+import { actionType } from "./context/reducer";
+
+const RootLayout = () => {
+  const [{ foodItems }, dispatch] = useStateValue();
+
+  const fetchData = async () => {
+    await getAllFoodItems().then((data) => {
+      dispatch({
+        type: actionType.SET_FOOD_ITEMS,
+        foodItems: data,
+      });
+    });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <AnimatePresence exitBeforeEnter>
+      <div className="w-screen h-auto flex flex-col bg-primary">
+        <Header />
+
+        <main className="mt-14 md:mt-20 px-4 md:px-16 py-4 w-full">
+          <Outlet />
+        </main>
+      </div>
+    </AnimatePresence>
+  );
+};
+
+export default RootLayout;
--- src/index.js
+++ src/index.js
@@ -2,18 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-import { BrowserRouter as Router } from "react-router-dom";
-
 import App from "./App";
 import { StateProvider } from "./context/StateProvider";
 import { initialState } from "./context/initalState";
 import reducer from "./context/reducer";
 
 ReactDOM.render(
-  <Router>
-    <StateProvider initialState={initialState} reducer={reducer}>
-      <App />
-    </StateProvider>
-  </Router>,
+  <StateProvider initialState={initialState} reducer={reducer}>
+    <App />
+  </StateProvider>,
+
   document.getElementById("root")
 );
